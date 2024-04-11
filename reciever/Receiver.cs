49536fd7f2980430c251236544caa31dc9ffbd31
Arun Mathew <arun.mathew@hrblock.com>
2024-04-11 11:10:35 +0530

    added rabbitmq receiver and sender

---
+++ reciever/Receiver.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "mystrongpassword"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "test-exchange",
+    type: ExchangeType.Direct,
+    durable: true,
+    autoDelete: false);
+
+var sys_queue = channel.QueueDeclare().QueueName;
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+channel.QueueBind(queue: sys_queue,
+    exchange: "test-exchange",
+    routingKey: "secret-message");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var enc = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(enc);
+    Console.WriteLine($" [x] Received :: {message}");
+    channel.BasicAck(deliveryTag: args.DeliveryTag, multiple: false);
+    Thread.Sleep(3000);
+};
+
+channel.BasicConsume(queue: sys_queue,
+    autoAck: false,
+    consumer: consumer);
+
+Console.ReadLine();
---
+++ sender/Sender.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "mystrongpassword"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "test-exchange",
+    type: ExchangeType.Direct,
+    durable: true,
+    autoDelete: false);
+
+var basicprops = channel.CreateBasicProperties();
+basicprops.Persistent = true;
+
+SendMessage();
+
+void SendMessage()
+{
+    //Console.WriteLine("enter your secret message !!");
+
+    for(int i = 0; i < 100; i++)
+    {
+        string message = $"secret message {i}!!";
+        var enc = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "test-exchange",
+            routingKey: "secret-message",
+            body: enc,
+            basicProperties: basicprops);
+
+        Console.WriteLine($" [{i}] Sent :: {message}");
+        Thread.Sleep(2000);
+    }
+
+    Console.ReadLine();
+    //Console.WriteLine("do you want to continue sending messages, if yes enter 'Y' !!");
+    //var inp = Console.ReadLine();
+    //if (inp.Equals("Y", StringComparison.OrdinalIgnoreCase))
+    //{
+    //    SendMessage();
+    //}
+}
